4f4283d1195276dd5f573933fc7b59a4eea66e1d
tsvetanv <tsvetan.vasilev@gmail.com>
2012-07-10 09:26:36 +0300

    Changed the name of servlet.

--- edu-gae/src/edu/gae/EduGAEServlet.java
+++ edu-gae/src/edu/gae/EduGAEServlet.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class Edu_gaeServlet extends HttpServlet {
+public class EduGAEServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, Cloud! I'm coming!");
 	}
 }
