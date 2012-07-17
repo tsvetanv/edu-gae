c0fa2a6f94f36fe1e485e0fd0c6e20032a24680d
Tsvetan Vasilev <tsvetan.vasilev@gmail.com>
2012-07-17 22:44:50 +0300

    Small change in Servlet.

--- edu-gae/src/edu/gae/EduGAEServlet.java
+++ edu-gae/src/edu/gae/EduGAEServlet.java
@@ -8,6 +8,6 @@ public class EduGAEServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, Cloud! I'm coming!");
+		resp.getWriter().println("Hello, Cloud! I'm coming! Forzaaa...");
 	}
 }
