850bdef87bbbc77b64714de5ebc8ca93044b2e32
Tsvetan Vasilev <tsvetan.vasilev@gmail.com>
2012-07-18 08:37:48 +0300

    Tested GAE UserService.

--- edu-gae/src/edu/gae/EduGAEServlet.java
+++ edu-gae/src/edu/gae/EduGAEServlet.java
@@ -3,11 +3,23 @@ package edu.gae;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 @SuppressWarnings("serial")
 public class EduGAEServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, Cloud! I'm coming! Forzaaa...");
+
+		UserService uSrvc = UserServiceFactory.getUserService();
+		User user = uSrvc.getCurrentUser();
+
+		if (user != null) {
+			resp.setContentType("text/plain");
+			resp.getWriter().println("Hello, " + user.getNickname() + "!");
+		} else {
+			resp.sendRedirect(uSrvc.createLoginURL(req.getRequestURI()));
+		}
 	}
 }
